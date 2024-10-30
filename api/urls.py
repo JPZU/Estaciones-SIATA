4a54b7530ff5f3c16b59e4ee88ae61163bedca6e
JPZU <juanpablozp@icloud.com>
2024-10-30 00:21:03 -0500

    Hice el calculo de la distancia + file: requirements.txt

--- api/urls.py
+++ api/urls.py
@@ -6,7 +6,11 @@ from api import views
 router = routers.DefaultRouter()
 # La 'r' es importante para que el enteinde que puede tener '/'
 router.register(r'estaciones', views.EstacionesViewSet)
+router.register(r'estaciones/cercanas',
+                views.CercanasViewSet, basename="cercanas")
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('estaciones/cercanas/<int:id>/', views.CercanasViewSet.as_view(
+        {'get': 'retrieve'}), name='cercanas-retrieve'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,11 +1,43 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from .serializer import EstacionesSerializer
 from .models import Estaciones
-
-# Create your views here.
+import math
+import pdb
 
 
 class EstacionesViewSet(viewsets.ModelViewSet):
     queryset = Estaciones.objects.all()
     # Crear esa clase
     serializer_class = EstacionesSerializer
+
+
+class CercanasViewSet(viewsets.ViewSet):
+    # Uso este método porque ese me permite hacer una busqueda por Id
+    def retrieve(self, request, pk=None):
+        # sacar la estación base usando el parámetro pk
+        estacion_base = Estaciones.objects.get(id=pk)
+        base_lat = estacion_base.ubicacion[0]
+        base_long = estacion_base.ubicacion[1]
+
+        # algoritmo: variables para encontrar la estación más cercana
+        estacion_mas_cercana = None
+        distancia_minima = float('inf')
+
+        for estacion in Estaciones.objects.exclude(id=pk):
+            lat = estacion.ubicacion[0]
+            long = estacion.ubicacion[1]
+
+            # distancia euclidiana
+            distancia = math.sqrt((lat - base_lat) **
+                                  2 + (long - base_long) ** 2)
+
+            # Obligo a poner el primer resultado como distancia minima
+            if distancia < distancia_minima:
+                distancia_minima = distancia
+                estacion_mas_cercana = estacion
+
+        # pdb.set_trace()
+        # Serializar el resultado
+        serializer = EstacionesSerializer(estacion_mas_cercana)
+        return Response(serializer.data)
